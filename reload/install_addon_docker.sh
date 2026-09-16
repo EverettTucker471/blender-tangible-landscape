e8e6815a7d3cab328ade891cb2f8e5a5e47f058d
EverettTucker471 <tucker24e@ncssm.edu>
2026-09-16 18:19:31 -0400

    Updating paths for newer docker container

--- reload/install_addon_docker.sh
+++ reload/install_addon_docker.sh
@@ -3,9 +3,9 @@
 # The coupling directory, we assume the development repo is inside
 ADDON_NAME="Blender for Tangible Landscape"
 COUPLING_DIR="/workspace/coupling"
+INSTALL_PATH="/tmp/tangible-landscape-install"
 WATCH_DIR=$COUPLING_DIR/Watch
 REPO_NAME="blender-tangible-landscape"
-PYTHON_PATH="/tmp/tangible-landscape-install/$REPO_NAME/reload/reinstall_addon.py"
 ZIP_PATH="$COUPLING_DIR/zip/blender-tangible-landscape-master.zip"
 
 # quitting current blender window - make sure to save first
@@ -22,7 +22,7 @@ mkdir -p $WATCH_DIR
 rm -f $COUPLING_DIR/zip/*.zip
 
 cd $COUPLING_DIR
-zip -r "$ZIP_PATH" $REPO_NAME -x "*/.*" "*/__pycache__/*" "*.pyc" "reload/*" "scratch/*" "Watch/*" "zip/*"
+zip -r "$ZIP_PATH" "$INSTALL_PATH/$REPO_NAME" -x "*/.*" "*/__pycache__/*" "*.pyc" "reload/*" "scratch/*" "Watch/*" "zip/*"
 
 # Installing the addon and configuring
-blender --background --python "$PYTHON_PATH" -- --addon_zip_path "$ZIP_PATH" --coupling_path "$WATCH_DIR" --addon_name "$REPO_NAME"
+blender --background --python "$INSTALL_PATH/$REPO_NAME/reload/reinstall_addon.py" -- --addon_zip_path "$ZIP_PATH" --coupling_path "$WATCH_DIR" --addon_name "$REPO_NAME"
